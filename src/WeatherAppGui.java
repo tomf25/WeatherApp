6921d25f61e1a92f1530e84480c7614c55b3ec5d
Tom Ferguson <tom.p.ferg@gmail.com>
2025-07-22 19:53:01 +0100

    window setup

--- src/WeatherAppGui.java
+++ src/WeatherAppGui.java
@@ -1,5 +1,16 @@
 import javax.swing.*;
 
 public class WeatherAppGui extends JFrame{
+    public WeatherAppGui(){
+        super("Weather App");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setSize(450,650);
+
+        setLocationRelativeTo(null); //Load our window to the center of the screen
+        setLayout(null); //manually position layout components
+        setResizable(false);
+        
+    }
 }
 
